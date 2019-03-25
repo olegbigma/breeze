8fff7d6b6aea218a4ba3710d56da3928f6a9a200
Oleh <oleg.bigma09@gmail.com>
2019-03-25 19:07:10 +0200

    last commit

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
-// document.getElementById('turnleft').onclick=turnleft;
-// var left=0;
-
-// function turnleft(){
-//     var str=document.getElementById('productslider');
-//     left=left-240;
-//     if(left<240){
-//         left=0;
-//     }
-//     str.style.left=left+'px';
-// }
-$('.productslider').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows:false,
-    dotsClass:'slick-dots',
-    mobileFirst:true,
-    responsive: [{
-        breakpoint: 1199 ,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-          infinite: true,
-               }
-      }]
-  });
+$(document).on('ready', function() {
+    $(".productslider").slick({
+        infinite: true,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows:true,
+        responsive: [
+            {
+            breakpoint: 550 ,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+                   },
+                  {
+                    breakpoint: 992 ,
+                    settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 1,
+                      infinite: true,
+                           }
+                  }       
+          ]
+    });
+});
